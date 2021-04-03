b0a7fe4129369718b14fe07866012cc4531a038b
woodwardtw <twwoodward@me.com>
2021-04-03 08:28:44 -0400

    revise acf structure, add voices post type

--- inc/custom-post-types.php
+++ inc/custom-post-types.php
@@ -65,7 +65,7 @@ function create_chapter_cpt() {
     'show_in_rest' => true,
     'publicly_queryable' => true,
     'capability_type' => 'post',
-    'menu_icon' => 'dashicons-universal-access-alt',
+    'menu_icon' => 'dashicons-book-alt',
   );
   register_post_type( 'Chapter', $args );
   
@@ -132,7 +132,7 @@ function create_lesson_cpt() {
     'show_in_rest' => true,
     'publicly_queryable' => true,
     'capability_type' => 'post',
-    'menu_icon' => 'dashicons-universal-access-alt',
+    'menu_icon' => 'dashicons-welcome-learn-more',
   );
   register_post_type( 'lesson', $args );
   
@@ -141,3 +141,70 @@ function create_lesson_cpt() {
   $wp_rewrite->flush_rules();
 }
 add_action( 'init', 'create_lesson_cpt', 0 );
+
+
+//voice custom post type
+
+// Register Custom Post Type voice
+// Post Type Key: voice
+
+function create_voice_cpt() {
+
+  $labels = array(
+    'name' => __( 'Voices', 'Post Type General Name', 'textdomain' ),
+    'singular_name' => __( 'Voice', 'Post Type Singular Name', 'textdomain' ),
+    'menu_name' => __( 'Voice', 'textdomain' ),
+    'name_admin_bar' => __( 'Voice', 'textdomain' ),
+    'archives' => __( 'Voice Archives', 'textdomain' ),
+    'attributes' => __( 'Voice Attributes', 'textdomain' ),
+    'parent_item_colon' => __( 'Voice:', 'textdomain' ),
+    'all_items' => __( 'All Voices', 'textdomain' ),
+    'add_new_item' => __( 'Add New Voice', 'textdomain' ),
+    'add_new' => __( 'Add New', 'textdomain' ),
+    'new_item' => __( 'New Voice', 'textdomain' ),
+    'edit_item' => __( 'Edit Voice', 'textdomain' ),
+    'update_item' => __( 'Update Voice', 'textdomain' ),
+    'view_item' => __( 'View Voice', 'textdomain' ),
+    'view_items' => __( 'View Voices', 'textdomain' ),
+    'search_items' => __( 'Search Voices', 'textdomain' ),
+    'not_found' => __( 'Not found', 'textdomain' ),
+    'not_found_in_trash' => __( 'Not found in Trash', 'textdomain' ),
+    'featured_image' => __( 'Featured Image', 'textdomain' ),
+    'set_featured_image' => __( 'Set featured image', 'textdomain' ),
+    'remove_featured_image' => __( 'Remove featured image', 'textdomain' ),
+    'use_featured_image' => __( 'Use as featured image', 'textdomain' ),
+    'insert_into_item' => __( 'Insert into voice', 'textdomain' ),
+    'uploaded_to_this_item' => __( 'Uploaded to this voice', 'textdomain' ),
+    'items_list' => __( 'Voice list', 'textdomain' ),
+    'items_list_navigation' => __( 'Voice list navigation', 'textdomain' ),
+    'filter_items_list' => __( 'Filter Voice list', 'textdomain' ),
+  );
+  $args = array(
+    'label' => __( 'voice', 'textdomain' ),
+    'description' => __( '', 'textdomain' ),
+    'labels' => $labels,
+    'menu_icon' => '',
+    'supports' => array('title', 'editor', 'revisions', 'author', 'trackbacks', 'custom-fields', 'thumbnail',),
+    'taxonomies' => array('category', 'post_tag'),
+    'public' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'menu_position' => 5,
+    'show_in_admin_bar' => true,
+    'show_in_nav_menus' => true,
+    'can_export' => true,
+    'has_archive' => true,
+    'hierarchical' => false,
+    'exclude_from_search' => false,
+    'show_in_rest' => true,
+    'publicly_queryable' => true,
+    'capability_type' => 'post',
+    'menu_icon' => 'dashicons-megaphone',
+  );
+  register_post_type( 'voice', $args );
+  
+  // flush rewrite rules because we changed the permalink structure
+  global $wp_rewrite;
+  $wp_rewrite->flush_rules();
+}
+add_action( 'init', 'create_voice_cpt', 0 );
